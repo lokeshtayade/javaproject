08f0f45f8ae400621f415d1addfad1f01d01b6b4
lokeshtayade <taydelokesh123@gmail.com>
2022-12-24 19:47:23 +0530

    added arraylist

---
+++ src/com/velocity/demo1/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package com.velocity.demo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Lokesh");
+		list.add("Bhagwan");
+		list.add("tayade");
+		System.out.println(list);
+	}
+}
